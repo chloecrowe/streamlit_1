737a4dce9ea77f213616ed6dd062dccbc83ddb31
Chloe <121978006+chloecrowe@users.noreply.github.com>
2023-03-20 11:10:16 +1300

    errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,19 +45,8 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice + add_my_fruit)
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
