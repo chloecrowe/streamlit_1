a3d4853a4e69d5a5619f92f8c19a285dce15fad3
Chloe <121978006+chloecrowe@users.noreply.github.com>
2023-03-20 11:30:44 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,4 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('The user added ', add_my_fruit)
 
-my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
